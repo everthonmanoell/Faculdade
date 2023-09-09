c16cda8b009cf39579cd68de516f760bc4367cfa
Everthon Manoel <112762040+everthonmanoell@users.noreply.github.com>
2023-09-08 22:30:43 -0300

    Delete uzzeWebconect2 directory

--- uzzeWebconect2/includes/footer.php
+++
@@ -1,5 +0,0 @@
-    <footer class="class.footer-container">
-        <p>Uzze - 2023 Projeto de Desenvolvimento Web - 2º Período</p>
-    </footer>
-</body>
-</html>
--- uzzeWebconect2/sobrenos.php
+++
@@ -1,8 +0,0 @@
-    <?php include __DIR__."/includes/header.php"; ?>
-    <main>
-        <section>
-
-            <?php include __DIR__."/includes/sobrenos.php";?>
-        </section>
-    </main>
-    <?php include __DIR__."/includes/footer.php"; ?>
